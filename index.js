b261fce4f8fb38478bd4ab78d6cf2fd7fdf522d4
Calvin <67180946+deleteme0@users.noreply.github.com>
2023-06-01 21:36:48 +0530

    3.8

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ app.use(express.json())
 
 var morgan = require('morgan')
 
-app.use(morgan('tiny'))
+morgan.token('logy', (req,res) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :logy'))
 
 let persons = [
     { 
