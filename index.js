d8b4a28d67438c1c6712da8f0a55ac636e75318d
Calvin <67180946+deleteme0@users.noreply.github.com>
2023-06-08 20:54:43 +0530

    3.17 and 3.18
    made everything work

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ morgan.token('logy', (req,res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :logy'))
 
+
 app.get('/api/persons', (request, response) => {
 
   Person.find({}).then(persons => {
@@ -23,54 +24,100 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
+
 app.get('/api/persons/:i',(request,response) => {
   const i = request.params.i;
 
-  const persons = Person.find({})
+  Person.find({})
+    .then(persons => {
+      const foun = persons.find((each)=> each._id == i)
 
-  const i_person = persons.find((each) => each.id == i)
-
-  response.json(i_person)
+      if (foun){
+        response.json(foun)
+      }else{
+        response.send("Not Found")
+      }
+    }
+  )
 })
+
 
 app.delete('/api/persons/:i',(request,response) =>{
   const  i = request.params.i;
   Person.findByIdAndRemove(i).then(()=>{
     response.status(204).end()
-  }).catch(()=>{
-    response.send("<p>Id not found</p>");
-    response.status(204).end()
-  })
-
+  }).catch(error => next(error))
 
   //persons= persons.filter((each) => each.id != i)
-
   
+  response.status(204).end()
+})
+
+
+app.put('/api/persons/:i',(request,response) => {
+  const req_person = request.body;
+
+  const new_person = {
+    name: req_person.name,
+    number: req_person.number
+  }
+
+  Person.findByIdAndUpdate(request.params.i,new_person,{new:true})
+    .then(each =>{
+      console.log(each)
+    })
+    .catch(error => next(error))
+
   response.status(204).end()
 })
 
 app.post('/api/persons',(request,response) => {
   const req_person = request.body
 
-  const persons = Person.find({})
 
   const person = new Person({
     name: req_person.name,
     number: req_person.number
   })
 
+
   person.save().then(result =>{
     console.log(result)
   })
 
-
   response.status(204).end()
 })
+
+
 app.get('/info',(request,response)=>{
   let d = Date(Date.now()).toString()
   
+  Person.find({}).then(persons => {  
   response.send("<div><b><p>Phonebook has info for "+ persons.length+" people</p><p> "+d+"</p></b></div>")
+}).catch(error => next(error))
+
 })
+
+
+//errorHandling
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
